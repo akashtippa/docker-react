d4a37780841ee86cd77106e37413824d4a29481a
root <root@ip-172-31-23-132.us-west-2.compute.internal>
2020-01-05 09:10:03 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is My First React App Deployment
         </p>
         <a
           className="App-link"
